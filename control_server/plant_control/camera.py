e1be80ca0bf2afe910219eab939e5dd15e8a4740
Felipe Navarro Balbino Alves <fnbalves@gmail.com>
2024-04-21 11:41:49 -0300

    feat: stream for more users

--- control_server/plant_control/camera.py
+++ control_server/plant_control/camera.py
@@ -7,17 +7,42 @@ class CameraHandler:
     
     def __init__(self):
         self.open_video()
+        self.subscribers = []
         
     def open_video(self):
         self.video_handler = cv2.VideoCapture(settings.CAMERA_INDEX)
         self.video_handler.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
         self.video_handler.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+        self.capture_thread = threading.Thread(
+        target=self.update_subscribers, args=())
+        self.capture_thread.start()
         
     def fetch_frame(self):
         grabbed, frame  = self.video_handler.read()
         if grabbed:
             _, frame = cv2.imencode('.jpg', frame)
         return grabbed, frame
+    
+    def add_subscriber(self, subscriber):
+        self.subscribers.append(subscriber)
+        
+    def update_subscribers(self):
+        while True:                        
+            grabbed, frame = self.fetch_frame()
+            if not grabbed:
+                continue
+            payload = frame.tobytes()
+            data = b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + payload + b'\r\n\r\n'
+            
+            to_remove = []
+            for sub in self.subscribers:
+                try:
+                    sub.send(data)
+                except:
+                    to_remove.add(sub)
+        
+            for sub in to_remove:
+                self.subscribers.remove(sub)
         
     @staticmethod
     def get_instance():
--- control_server/plant_control/views.py
+++ control_server/plant_control/views.py
@@ -28,22 +28,24 @@ class CameraStream(APIView):
     
     @staticmethod
     def image_gen():
-        print('OPENED STREAM')
-        camera_handler = CameraHandler.get_instance()
         while True:
             try:            
-                grabbed, frame = camera_handler.fetch_frame()
-                if not grabbed:
-                    continue
-                payload = frame.tobytes()
-                yield(b'--frame\r\n'
-                  b'Content-Type: image/jpeg\r\n\r\n' + payload + b'\r\n\r\n')
+                frame_data = yield
+                yield frame_data
             except:
                 print('Exiting image generator')
                 break
+    
+    @staticmethod
+    def open_image_gen():
+        camera_handler = CameraHandler.get_instance()
+        gen = CameraStream.image_gen()
+        camera_handler.add_subscriber(gen)
+        return gen
+        
     @method_decorator(gzip.gzip_page)            
     def get(self, *args, **kwargs):
-        return StreamingHttpResponse(CameraStream.image_gen(), content_type="multipart/x-mixed-replace;boundary=frame")
+        return StreamingHttpResponse(CameraStream.open_image_gen(), content_type="multipart/x-mixed-replace;boundary=frame")
             
 @method_decorator(user_passes_test(test_user_authenticated), name='dispatch')       
 class ControlPage(TemplateView):
